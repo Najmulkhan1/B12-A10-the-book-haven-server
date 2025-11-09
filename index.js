24d486e29c87f28a4632a14117afbd72eddd13a5
Najmulkhan1 <najmulislam624@gmail.com>
2025-11-10 01:41:44 +0600

    latest-books api make

--- index.js
+++ index.js
@@ -42,6 +42,11 @@ const run = async () => {
             res.send(result)
         })
 
+        app.get('/latest-books', async(req,res) => {
+            const result = await booksCollection.find().sort({created_at: "desc"}).limit(6).toArray()
+            res.send(result)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
